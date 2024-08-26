bed5e9e297291f2bf34238e2c8d60455bf07b68a
wiktor <wiktorlem1@wp.pl>
2024-08-26 19:03:07 +0200

    zmieniona struktura html i mobile header

---
+++ src/resources/script.js
@@ -0,0 +1,5 @@
+let burger = document.querySelector('.burger-menu');
+burger.addEventListener('click', () => {
+
+    document.querySelector('.main-header_navigation').classList.toggle('mobile');
+})
